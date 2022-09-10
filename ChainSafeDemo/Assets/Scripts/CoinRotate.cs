a9bbea1ec6fec843fc3c809b3150dff9419c7159
sneakzttv <sneakz@chainsafe.io>
2022-09-10 14:43:54 +0700

    images added
    images added

--- ChainSafeDemo/Assets/Scripts/CoinRotate.cs
+++ ChainSafeDemo/Assets/Scripts/CoinRotate.cs
@@ -6,7 +6,7 @@ public class CoinRotate : MonoBehaviour
 {
     void Update()
     {
-        // rotates object
+        // rotates object, you can change the numbers here to increase rotation speed
         gameObject.transform.Rotate(50 * Time.deltaTime, 0, 0);
     }
 }
